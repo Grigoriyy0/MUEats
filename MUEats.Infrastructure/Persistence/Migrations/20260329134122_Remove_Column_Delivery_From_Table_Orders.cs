58b57dac739a30a91563d5d1362e74b4210c5751
grigoriikokotkin <name.kokotkin@yandex.ru>
2026-03-29 21:41:43 +0800

    3. migration

---
+++ MUEats.Infrastructure/Persistence/Migrations/20260329134122_Remove_Column_Delivery_From_Table_Orders.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MUEats.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Remove_Column_Delivery_From_Table_Orders : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CourierId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "DeliverBefore",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "DeliveredAt",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "DeliveryStatus",
+                table: "Orders");
+
+            migrationBuilder.RenameColumn(
+                name: "Price",
+                table: "Orders",
+                newName: "TotalPrice");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TotalPrice",
+                table: "Orders",
+                newName: "Price");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "CourierId",
+                table: "Orders",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeliverBefore",
+                table: "Orders",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeliveredAt",
+                table: "Orders",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "DeliveryStatus",
+                table: "Orders",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
