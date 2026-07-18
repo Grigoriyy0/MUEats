04a844d40329d0f4759d2368def71149c0c8cbd6
grigoriikokotkin <name.kokotkin@yandex.ru>
2026-07-18 17:03:17 +0800

    remove attribute

--- src/core/MUEats.Api/Adapters/Http/ShoppingCartsController.cs
+++ src/core/MUEats.Api/Adapters/Http/ShoppingCartsController.cs
@@ -11,7 +11,6 @@ namespace MUEats.Adapters.Http;
 public class ShoppingCartsController(ShoppingCartsService shoppingCartsService) : ControllerBase
 {
     [HttpPost]
-    [Authorize(Roles = "User")]
     public async Task<IActionResult> CreateAsync([FromBody] AddFoodItemDto dto, CancellationToken ct)
     {
         try
@@ -33,7 +32,6 @@ public class ShoppingCartsController(ShoppingCartsService shoppingCartsService) 
 
     [HttpGet]
     [Route("{userId:guid}")]
-    [Authorize(Roles = "User")]
     public async Task<IActionResult> GetByIdAsync([FromRoute] Guid userId, CancellationToken ct)
     {
         var result = await shoppingCartsService.GetShoppingCartAsync(userId, ct);
@@ -48,7 +46,6 @@ public class ShoppingCartsController(ShoppingCartsService shoppingCartsService) 
 
     [HttpDelete]
     [Route("cart-items/{itemId:guid}")]
-    [Authorize(Roles = "User")]
     public async Task<IActionResult> DeleteCartItemAsync(Guid itemId, CancellationToken ct)
     {
         var result = await shoppingCartsService.DeleteCartItemAsync(itemId, ct);
